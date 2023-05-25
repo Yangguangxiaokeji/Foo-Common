f8c40268cd8b59aea57eff4edf221f268814ac7a
wangxin <wx1063140310@163.com>
2023-05-25 10:55:48 +0800

    feign调用增加header信息

--- Foogui-Cloud/foo-common/foo-common-core/src/main/java/com/foogui/foo/common/core/interceptor/FeignRequestInterceptor.java
+++ Foogui-Cloud/foo-common/foo-common-core/src/main/java/com/foogui/foo/common/core/interceptor/FeignRequestInterceptor.java
@@ -1,12 +1,35 @@
 package com.foogui.foo.common.core.interceptor;
 
+import com.alibaba.fastjson.JSON;
 import com.foogui.foo.common.core.constant.HttpConstant;
+import com.google.common.collect.Maps;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Map;
+
+/**
+ * 微服务调用，将用户信息和请求来源存入header中
+ *
+ * @author Foogui
+ * @date 2023/05/25
+ */
 public class FeignRequestInterceptor implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate template) {
+        //todo：从context获取用户信息存入
+        // 模拟
+        Map<String, String> user= Maps.newHashMap();
+        if(user != null){
+            try {
+                String userJson = JSON.toJSONString(user) ;
+                template.header(HttpConstant.USER_INFO,  URLEncoder.encode(userJson, "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException(e);
+            }
+        }
         template.header(HttpConstant.FROM_WHERE, HttpConstant.INNER);
     }
 }
