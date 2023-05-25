2d5b7b6ba10335da074091c0c8a589d527b19446
wangxin <wx1063140310@163.com>
2023-05-25 10:55:10 +0800

    增加缓存配置

--- Foogui-Cloud/foo-common/foo-common-core/src/main/java/com/foogui/foo/common/core/constant/CacheConstant.java
+++ Foogui-Cloud/foo-common/foo-common-core/src/main/java/com/foogui/foo/common/core/constant/CacheConstant.java
@@ -4,7 +4,7 @@ package com.foogui.foo.common.core.constant;
  * @author wangxin
  * @version $ Id: RedisConstant, v 0.1 2023/02/16 13:27 banma-0369 Exp $
  */
-public interface RedisConstant {
+public interface CacheConstant {
      final String LOGIN_TOKEN="login:token:";
      final String CAPTCHA_CODE ="captcha:code:";
      final String CAPTCHA_LOGIN ="captcha:login:";
